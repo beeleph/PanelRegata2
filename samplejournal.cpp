56eda4acfc86d0377623f5c33c08d57ad999a6d2
beeleph <isjironn@gmail.com>
2022-04-08 08:53:35 +0300

    sampleJournal fix

--- samplejournal.cpp
+++ samplejournal.cpp
@@ -9,7 +9,7 @@ sampleJournal::sampleJournal(QWidget *parent) :
     ui->setupUi(this);
     QSqlDatabase db = QSqlDatabase::database("NAA_db");
     model = new QSqlTableModel(nullptr, db);
-    model->setTable("Sample");
+    model->setTable("table_Sample");
     model->setEditStrategy(QSqlTableModel::OnManualSubmit);
     model->select();
     model->setHeaderData(0, Qt::Horizontal, tr("Код страны"));
@@ -18,7 +18,7 @@ sampleJournal::sampleJournal(QWidget *parent) :
     sampleInfo.resize(7);   // 0 - which table (sli/lli), 1 - country code, 2 - Client_ID, 3 - Year, 4 - Sample_Set_ID, 5 - Sample_ID, 6 - Date_Start,
     on_sliButton_clicked();
     ui->comboBox->clear();
-    QSqlQuery query("SELECT DISTINCT P_Date_Sample_Preparation FROM Sample ORDER BY P_Date_Sample_Preparation", db);
+    QSqlQuery query("SELECT DISTINCT P_Date_Sample_Preparation FROM table_Sample ORDER BY P_Date_Sample_Preparation", db);
     while (query.next()){
         ui->comboBox->addItem(query.value(0).toString());
     }
@@ -39,11 +39,15 @@ void sampleJournal::on_cancelButton_clicked()
 void sampleJournal::on_sliButton_clicked()
 {
     sampleInfo[0] = "КЖИ";
+    ui->sliButton->setChecked(true);
+    ui->lliButton->setChecked(false);
 }
 
 void sampleJournal::on_lliButton_clicked()
 {
     sampleInfo[0] = "ДЖИ";
+    ui->sliButton->setChecked(false);
+    ui->lliButton->setChecked(true);
 }
 
 void sampleJournal::updateTable(QString dateStart){
